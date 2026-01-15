00f660384a9ba64aa12a6ddf589e86fe64fa8d4f
Bishal Budhathoki <deverbishal331@gmail.com>
2026-01-16 02:19:09 +1100

    Training & Compliance Hub api added

--- routes/trainingComplianceRoutes.js
+++ routes/trainingComplianceRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/trainingComplianceController');
-const auth = require('../middleware/auth');
+const { authenticateUser: auth } = require('../middleware/auth');
 const { upload } = require('../config/storage');
 
 // Certifications
