283dcaaea0293967e8675062d505a65515dedcc4
Bishal Budhathoki <deverbishal331@gmail.com>
2026-01-26 19:00:49 +1100

    fix: update complianceRoutes and multiOrgRoutes to use authenticateUser middleware

--- routes/multiOrgRoutes.js
+++ routes/multiOrgRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const multiOrgController = require('../controllers/multiOrgController');
-const { protect } = require('../middleware/auth');
+const { authenticateUser } = require('../middleware/auth');
 
-router.get('/rollup', protect, multiOrgController.getRollup);
+router.get('/rollup', authenticateUser, multiOrgController.getRollup);
 
 module.exports = router;
