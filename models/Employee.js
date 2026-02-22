e8bc866c5ec61b8ecea5c0ebc61fc28805058b45
Bishal Budhathoki <deverbishal331@gmail.com>
2026-02-23 02:13:48 +1100

    Fix backend test bootstrap imports for tracking and workforce

---
+++ models/Employee.js
@@ -0,0 +1,5 @@
+/**
+ * Compatibility alias for legacy/new services that import `models/Employee`.
+ * The app stores workforce records in the `User` model/collection.
+ */
+module.exports = require('./User');
